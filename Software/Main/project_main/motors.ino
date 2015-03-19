d45d273b63cae714ee8e37b9a44645c155a29461
D Murphy <dan.murphy101@gmail.com>
2015-03-19 04:24:58 -0400

    generic motor move
    accepts +ve and -ve ints for each side and writes them to fwd and
    reverse respectively

--- Software/Main/project_main/motors.ino
+++ Software/Main/project_main/motors.ino
@@ -39,12 +39,33 @@ void motor_rev(int pwm)
    analogWrite(LT_REV, pwm);
 }
 
-void motor_right(int pwm_lt, int pwm_rt)
+void motor_on(int pwm_lt, int pwm_rt)
 {
-  analogWrite(RT_FWD, 0);
-  analogWrite(LT_REV, 0);
-  analogWrite(RT_REV, pwm_rt);
-  analogWrite(LT_FWD, pwm_lt);
+  //accepts two ints for the two sides
+  //-ve values write to reverse pins, 
+  //+ve values write to forward pins
+  
+  if (pwm_lt < 0)
+  {
+    analogWrite(LT_FWD, 0);
+    analogWrite(LT_REV, pwm_lt);
+  }
+  else
+  {
+    analogWrite(LT_REV, 0);
+    analogWrite(LT_FWD, pwm_lt);
+  }
+  
+  if (pwm_rt < 0)
+  {
+    analogWrite(RT_FWD, 0);
+    analogWrite(RT_REV, pwm_lt);
+  }
+  else
+  {
+    analogWrite(RT_REV, 0);
+    analogWrite(RT_FWD, pwm_lt);
+  }
 }
 
 void motor_left(int pwm_lt, int pwm_rt)
@@ -53,36 +74,6 @@ void motor_left(int pwm_lt, int pwm_rt)
   analogWrite(LT_FWD, 0);
   analogWrite(RT_FWD, pwm_rt);
   analogWrite(LT_REV, pwm_lt);
-}
-
-void adjust_dr_right(int pwm, int k)
-{
-  analogWrite(RT_FWD, 0);
-  delay(int(k));
-  analogWrite(RT_FWD,pwm);
-  
-}
-
-void adjust_dr_left(int pwm, int k)
-{
-  analogWrite(LT_FWD, 0);
-  delay(int(k));
-  analogWrite(LT_FWD,pwm);
-}
-
-void adjust_right()
-{
-  analogWrite(LT_REV, 150);
-  delay(500);
-  analogWrite(LT_REV, 0);
-}
-
-void adjust_left()
-{
-  analogWrite(RT_REV, 150);
-  delay(500);
-  analogWrite(RT_REV, 0);
-  
 }
 
 
